2130cebe4b301278c15177a391ff9caa280dc164
Alexander Shiryaev <alex_sh_v@mail.ru>
2021-05-28 22:05:19 +0300

    img fix

--- components/Offer/EmptyOffer.jsx
+++ components/Offer/EmptyOffer.jsx
@@ -3,12 +3,8 @@ import Image from 'next/image';
 const EmptyOffer = () => {
   return (
     <>
-      <Image
-        src='/public/man.svg'
-        alt='man-img'
-        width={500}
-        height={500}
-      />
+      <h1 className='offer__title'>Здесь будут находиться результаты вашего поиска</h1>
+      <Image src='/man.svg' alt='man-img' width={500} height={500} />
     </>
   );
 };
