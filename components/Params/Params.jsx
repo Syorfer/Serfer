e437c57ca9bbc2ee6ce424ccaf260b4727c66b3b
Alexander Shiryaev <alex_sh_v@mail.ru>
2021-05-30 21:55:40 +0300

    added material-ui, added sliders, styles update

--- components/Params/Params.jsx
+++ components/Params/Params.jsx
@@ -1,7 +1,23 @@
+import CustomSlider from '../Sliders/CustomSlider';
+import Typography from '@material-ui/core/Typography';
+import RatingSlider from '../Sliders/RatingSlider';
+
 const Params = () => {
-    return ( 
-        <h1>Параметры</h1>
-     );
-}
- 
+  return (
+    <div className='params'>
+      <Typography variant='h4' gutterBottom>
+        Параметры
+      </Typography>
+      <div className='params__price'>
+        <CustomSlider>Цена</CustomSlider>
+        <CustomSlider>Доставка</CustomSlider>
+        <RatingSlider>Рейтинг</RatingSlider>
+        <Typography id='discrete-slider' variant='h5' gutterBottom>
+          Отзывы
+        </Typography>
+      </div>
+    </div>
+  );
+};
+
 export default Params;
