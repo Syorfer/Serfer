bb3b726c30e3bd01eb604afa47ab294a2e1e22f5
AndreSmrnv <tehmir@bk.ru>
2021-06-14 20:48:45 +0300

    move pages -> src/pages

---
+++ src/components/Offer/OfferWResult/OfferWResult.jsx
@@ -0,0 +1,17 @@
+import { Grid } from '@material-ui/core';
+import Image from 'next/image';
+
+const OfferWResult = () => {
+  return (
+    <>
+      <h1 className='offer__title'>
+        Здесь будут находиться результаты вашего поиска
+      </h1>
+      <Grid container justify='center'>
+        <Image src='/man.svg' alt='man-img' width={550} height={550} />
+      </Grid>
+    </>
+  );
+};
+
+export default OfferWResult;
