ce0ec52abe8f298dcaba5f2eef40189bb0aa7c12
AndreSmrnv <tehmir@bk.ru>
2021-06-15 08:44:25 +0300

    change init_param

--- src/constants/initParam.js
+++ src/constants/initParam.js
@@ -4,15 +4,30 @@ export const REVIEWS = [
   'Не имеет значения'
 ];
 
+const range = {
+  price: {
+    min: 1000,
+    max: 5000
+  },
+  delivery: {
+    min: 50,
+    max: 800
+  },
+  rate: {
+    min: 1,
+    max: 5
+  }
+}
+
 export const INITIAL_PARAM = {
-    priceMin: 1000,
-    priceMax: 5000,
-    priceValue: [1000,5000],
-    deliveryMin: 50,
-    deliveryMax: 800,
-    deliveryValue: [50,800],
-    rateMin: 1,
-    rateMax: 5,
-    rateValue: 0,
+    priceMin: range.price.min,
+    priceMax: range.price.max,
+    priceValue: [range.price.min,range.price.max],
+    deliveryMin: range.delivery.min,
+    deliveryMax: range.delivery.max,
+    deliveryValue: [range.delivery.min,range.delivery.max],
+    rateMin: range.rate.min,
+    rateMax: range.rate.max,
+    rateValue: range.rate.min,
     review: REVIEWS[0]
   };
