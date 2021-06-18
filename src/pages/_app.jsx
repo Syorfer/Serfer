6aa60b959e611334a00cae7881ed353601393b4b
AndreSmrnv <tehmir@bk.ru>
2021-06-18 13:21:04 +0300

    move search context to _app

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -5,6 +5,8 @@ import { CacheProvider } from '@emotion/react';
 import { CssBaseline } from '@material-ui/core';
 import createCache from '@emotion/cache';
 //import { useStore } from '@/redux/store/store';
+import { SearchContext } from '@/contexts/searchContext';
+import { INITIAL_SEARCH } from '@/constants/initSearch'
 import theme from '@/src/theme';
 import '@/styles/main.scss';
 
@@ -22,18 +24,18 @@ export default function MyApp({ Component, pageProps }) {
       jssStyles.parentElement.removeChild(jssStyles);
     }
   }, []);
-
+  const [search, setSearch] = useState(INITIAL_SEARCH);
   
 
   return (
     <CacheProvider value={cache}>
       {/* <Provider store={store}> */}
-      
+      <SearchContext.Provider value={{ search, setSearch }}>
         <ThemeProvider theme={theme}>
           <CssBaseline />
           <Component {...pageProps} />
         </ThemeProvider>
-      
+      </SearchContext.Provider>
       {/* </Provider> */}
     </CacheProvider>
   );
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,13 +1,12 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import { MainLayout } from '@/components/MainLayout';
 import OfferLayout from '@/components/Offer/OfferLayout';
 import Params from '@/components/Params';
 import { SearchContext } from '@/contexts/searchContext';
-import { INITIAL_SEARCH } from '@/constants/initSearch'
 import { GoodsContext } from '@/contexts/goodsContext';
-export default function Index() {
-  const [goods, setGoods] = useState([]);
-  const [search, setSearch] = useState(INITIAL_SEARCH);
+export default function Index({ dataGoods }) {
+  const [goods, setGoods] = useState(dataGoods);
+  const { search } = useContext(SearchContext);
   useEffect(() => {
     async function load() {
       const response = await fetch(`${process.env.API_URL}/goods`);
@@ -16,14 +15,24 @@ export default function Index() {
     }
     load();
   }, []);
+
   return (
     <GoodsContext.Provider value={{ goods, setGoods }}>
-      <SearchContext.Provider value={{ search, setSearch }}>
-        <MainLayout title={search.searchValue}>
-          <Params />
-          <OfferLayout />
-        </MainLayout>
-      </SearchContext.Provider>
-    </GoodsContext.Provider>  
+
+      <MainLayout title={search.searchValue}>
+        <Params />
+        <OfferLayout />
+      </MainLayout>
+
+    </GoodsContext.Provider>
   );
+}
+
+Index.getInitialProps = async () => {
+  const response = await fetch(`${process.env.API_URL}/goods`)
+  const dataGoods = await response.json()
+
+  return {
+    dataGoods
+  }
 }
